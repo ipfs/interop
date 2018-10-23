21bf7669305dcc924221aeee2cc774d238338870
Alan Shaw <alan.shaw@protocol.ai>
2018-10-23 13:10:46 +0100

    fix: revert to old waitFor timeout
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- test/pubsub.js
+++ test/pubsub.js
@@ -17,7 +17,7 @@ const DaemonFactory = require('ipfsd-ctl')
  * Wait for a condition to become true.  When its true, callback is called.
  */
 function waitFor (predicate, callback) {
-  const ttl = Date.now() + (50 * 1000)
+  const ttl = Date.now() + (10 * 1000)
   const self = setInterval(() => {
     if (predicate()) {
       clearInterval(self)
