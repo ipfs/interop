b271ea810d4a79a1fdfab58533e1afe29475ebe3
Vasco Santos <vasco.santos@ua.pt>
2018-10-24 10:18:51 +0100

    fix: files and pubsub ci (#37)
    License: MIT
    Signed-off-by: Alan Shaw <alan.shaw@protocol.ai>

--- test/utils/daemon.js
+++ test/utils/daemon.js
@@ -1,32 +1,16 @@
 'use strict'
 
-const os = require('os')
-const path = require('path')
-const hat = require('hat')
-const waterfall = require('async/waterfall')
 const DaemonFactory = require('ipfsd-ctl')
 const goDf = DaemonFactory.create()
 const jsDf = DaemonFactory.create({ type: 'js' })
 
 const spawnInitAndStartDaemon = (factory) => {
-  const dir = path.join(os.tmpdir(), hat())
-  let instance
-
   return new Promise((resolve, reject) => {
-    waterfall([
-      (cb) => factory.spawn({
-        repoPath: dir,
-        disposable: false,
-        initOptions: {
-          bits: 1024
-        }
-      }, cb),
-      (node, cb) => {
-        instance = node
-        instance.init(cb)
-      },
-      (cb) => instance.start((error) => cb(error, instance))
-    ], (error) => {
+    factory.spawn({
+      initOptions: {
+        bits: 1024
+      }
+    }, (error, instance) => {
       if (error) {
         return reject(error)
       }
