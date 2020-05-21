a6ee0d70f919b97f007a62ab35137962a0e239f4
Hugo Dias <hugomrdias@gmail.com>
2020-05-21 11:37:09 +0100

    fix: use cid instead of hash

--- test/pin.js
+++ test/pin.js
@@ -134,7 +134,7 @@ describe('pin', function () {
       expect(goPins).to.deep.include.members(jsPins)
       expect(jsPins).to.deep.include.members(goPins)
 
-      const dirPin = goPins.find(pin => pin.hash === jupiterDir.hash)
+      const dirPin = goPins.find(pin => pin.cid.equals(jupiterDir.cid))
       expect(dirPin.type).to.eql('indirect')
     })
   })
