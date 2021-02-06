12ada7c21773723ccb2a515ccc88edd7f311b4a0
Hugo Dias <hugomrdias@gmail.com>
2021-02-06 13:18:41 +0000

    fix: fix node loading  1

--- test/utils/daemon-factory.js
+++ test/utils/daemon-factory.js
@@ -3,13 +3,26 @@
 const { createFactory } = require('ipfsd-ctl')
 const isNode = require('detect-node')
 
+let ipfsHttpModule
+let ipfsModule
+try {
+  ipfsHttpModule = require(process.env.IPFS_JS_HTTP_MODULE)
+} catch (err) {
+  ipfsHttpModule = require('ipfs-http-client')
+}
+
+try {
+  ipfsModule = require(process.env.IPFS_JS_MODULE)
+} catch (err) {
+  ipfsModule = require('ipfs')
+}
 module.exports = createFactory({
   type: 'go',
   test: true,
-  ipfsHttpModule: require.resolve(`${process.env.IPFS_JS_HTTP_MODULE}`)
+  ipfsHttpModule
 }, {
   proc: {
-    ipfsModule: require.resolve(`${process.env.IPFS_JS_MODULE}`)
+    ipfsModule
   },
   js: {
     ipfsBin: isNode ? process.env.IPFS_JS_EXEC || require.resolve(`${process.env.IPFS_JS_MODULE || 'ipfs'}/src/cli.js`) : undefined
