1ffe4d056dabe0c0864840e083b32a3aedd6d57a
Marcin Rataj <lidel@lidel.org>
2021-11-29 20:05:24 +0100

    chore: use go-ipfs master

--- scripts/custom-runtime.sh
+++ scripts/custom-runtime.sh
@@ -25,7 +25,7 @@ if [ ! -d /tmp/go-ipfs ]; then
         git clone https://github.com/ipfs/go-ipfs.git
         cd go-ipfs
             # set implementation to specific commit
-            git checkout 8c902b156b1c35d17ce45de2525c80bf71c77b5b # https://github.com/ipfs/go-ipfs/pull/8563
+            git switch master # https://github.com/ipfs/go-ipfs/pull/8563 got merged, lets roll with master until go-ipfs 0.11.0-rc1
             make build
 fi
 fi
