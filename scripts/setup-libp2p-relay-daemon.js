7b14b56ba9b1ddb019385040d5908b11e476387b
achingbrain <alex@achingbrain.net>
2022-09-09 19:18:17 +0100

    chore: should be exectutable

--- scripts/setup-libp2p-relay-daemon.js
+++ scripts/setup-libp2p-relay-daemon.js
@@ -143,11 +143,6 @@ async function download ({ version, platform, arch, installPath, distUrl }) {
   fs.rmSync(tmpDir, { recursive: true })
   console.info(`Unpacked binary placed in ${finalPath}`)
 
-  if (platform !== 'windows') {
-    console.info(`Ensuring ${finalPath} is executable`)
-    fs.chmodSync(finalPath, 0o755)
-  }
-
   return finalPath
 }
 
