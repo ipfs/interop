6295ef4a48cc7a104b700be2397f5508a6543bf6
achingbrain <alex@achingbrain.net>
2022-09-10 09:19:49 +0100

    chore: actually time out

--- test/utils/relayd.js
+++ test/utils/relayd.js
@@ -1,8 +1,10 @@
-import { isNode } from 'wherearewe'
+import { isNode, isElectronMain } from 'wherearewe'
 import fs from 'fs'
 import path from 'path'
 import { execaCommand } from 'execa'
 import pTimeout from 'p-timeout'
+import { download, LIBP2P_RELAY_DAEMON_VERSION } from '../../scripts/download-relay-daemon.js'
+import os from 'os'
 // @ts-expect-error no types
 import goenv from 'go-platform'
 const platform = process.env.TARGET_OS || goenv.GOOS
@@ -18,14 +20,41 @@ const RELAY_STARTUP_TIMEOUT = Number(process.env.RELAY_STARTUP_TIMEOUT || 30000)
  * @param {number} version
  */
 export async function getRelayV (version) {
-  if (!isNode) return
-  if (relays.has(version)) return relays.get(version)
-  if (process.env.DEBUG) console.log(`Starting relayd_v${version}..`) // eslint-disable-line no-console
-  if (version < 1 || version > 2) throw new Error('Unsupported circuit relay version')
+  if (!isNode && !isElectronMain) {
+    return
+  }
+
+  if (relays.has(version)) {
+    return relays.get(version)
+  }
+
+  if (process.env.DEBUG) {
+    console.log(`Starting relayd_v${version}..`) // eslint-disable-line no-console
+  }
+
+  if (version < 1 || version > 2) {
+    throw new Error('Unsupported circuit relay version')
+  }
+
   const binaryPath = path.join('scripts', `libp2p-relay-daemon${platform === 'windows' ? '.exe' : ''}`)
   const configPath = path.join('scripts', `relayd_v${version}.config.json`)
-  const identityPath = path.join('scripts', `relayd_v${version}-${Math.random()}.identity`)
-  if (process.env.DEBUG) console.info(`${binaryPath} -config ${configPath} -id ${identityPath}`) // eslint-disable-line no-console
+  const identityPath = path.join(os.tmpdir(), `relayd_v${version}-${Math.random()}.identity`)
+
+  if (!fs.existsSync(binaryPath)) {
+    console.info('libp2p-relay-daemon binary not found at', binaryPath) // eslint-disable-line no-console
+    await download({
+      version: LIBP2P_RELAY_DAEMON_VERSION,
+      platform: process.env.TARGET_OS || goenv.GOOS || os.platform(),
+      arch: process.env.TARGET_ARCH || goenv.GOARCH || os.arch(),
+      distUrl: process.env.GO_IPFS_DIST_URL || 'https://dist.ipfs.io',
+      installPath: path.resolve('scripts')
+    })
+  }
+
+  if (process.env.DEBUG) {
+    console.info(`${binaryPath} -config ${configPath} -id ${identityPath}`) // eslint-disable-line no-console
+  }
+
   const relayd = execaCommand(`${binaryPath} -config ${configPath} -id ${identityPath}`, {
     all: true
   })
@@ -56,7 +85,14 @@ export async function getRelayV (version) {
     }
   }
 
-  const id = await pTimeout(waitForStartup(), {
+  const promise = waitForStartup()
+  // @ts-ignore
+  promise.cancel = () => {
+    console.error(`Timed out waiting for ${binaryPath} to start after ${RELAY_STARTUP_TIMEOUT}ms, killing process`) // eslint-disable-line no-console
+    relayd.kill()
+  }
+
+  const id = await pTimeout(promise, {
     milliseconds: RELAY_STARTUP_TIMEOUT
   })
 
